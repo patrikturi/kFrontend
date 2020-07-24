07b21a092cb880098b626f094c463fe69fae060d
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-07-24 23:17:22 +0200

    Rephrase welcome text

--- src/components/public/LandingPage.tsx
+++ src/components/public/LandingPage.tsx
@@ -13,25 +13,17 @@ export const LandingPage = () => {
                     <b>kSoccer</b> is a football mod in the game SecondLife.
                     You can customize and play with your own football palyer.
                     Be part of a team and compete for trophies.
-                    Make your name among other players and become a superstar.
+                    Make your name among other players and become a superstar!
                 </p>
-                <p className="lead pt-5">
-                <a href="/getting-started#secondlife">What is SecondLife?</a>
+                <p className="lead text-left">
+                    Are you...
+                    <ul>
+                        <li>new? <br /><a href="/getting-started#secondlife">Get Started in SecondLife here</a></li>
+                        <li>a SecondLife player? <br /> <a href="/getting-started#ksoccer">Get Started with kSoccer here</a></li>
+                        <li>a kSoccer player? <br /><a href="/register">Register now!</a><br />
+                        <small>or <a href="/login">Log in</a> if you already have an account</small></li>
+                    </ul>
                 </p>
-
-                <p className="lead">
-                <a href="/getting-started#ksoccer">What is kSoccer?</a>
-                </p>
-
-                <button type="button" className="btn btn-success btn-lg">Register now!</button>
-
-            {/*TODO:
-                Are you...
-                 new? => <Get Started in SecondLife>
-                 a SecondLife player? => <Get Started with kSoccer>
-                 a kSoccer player? => <Register here>
-                    (or Log In if you already registered)
-            */}
             </div>
         </>
     );

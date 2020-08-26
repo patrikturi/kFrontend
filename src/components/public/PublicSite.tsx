f5fd802e1bccd4633a58d5d740030392396de9f1
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-08-27 01:44:47 +0200

    feat: remove top header

--- src/components/public/PublicSite.tsx
+++ src/components/public/PublicSite.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react';
-import {TopHeader} from './TopHeader';
 import {Navbar} from './Navbar';
 import {PageBase} from './PageBase';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -8,7 +7,6 @@ export const PublicSite = () => {
     return (
         <div className="container-fluid height-full bg-light px-0">
           <Router>
-            <TopHeader />
             <Suspense fallback="loading">
               <Navbar />
             </Suspense>

c344d94acca11ed23ac3a43594e0010eed7e88a2
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-10-31 23:04:58 +0100

    cleanup: league site

--- src/components/public/PageBase.tsx
+++ src/components/public/PageBase.tsx
@@ -3,12 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { LandingPage } from './LandingPage';
 import { GettingStartedPage } from './GettingStartedPage';
 import { UpdatesPage } from './UpdatesPage';
-import { ResultsPage } from '../league/ResultsPage';
-import { StandingsPage } from '../league/StandingsPage';
-import { TopScorersPage } from '../league/TopScorersPage';
-import { RulesPage } from '../league/RulesPage';
-import { SchedulePage } from '../league/SchedulePage';
-import { MediaPage } from '../league/MediaPage';
 
 import { RegistrationForm } from './RegistrationForm';
 import { LoginForm } from './LoginForm';
@@ -26,15 +20,6 @@ export const PageBase: React.FC = () => {
         <Route path="/updates" component={UpdatesPage} />
         <Route path="/login/" component={LoginForm} />
         <Route path="/register" component={RegistrationForm} />
-        <Route path="/leagues/:leagueId/results" component={ResultsPage} />
-        <Route path="/leagues/:leagueId/standings" component={StandingsPage} />
-        <Route path="/leagues/:leagueId/schedule" component={SchedulePage} />
-        <Route
-          path="/leagues/:leagueId/top-scorers"
-          component={TopScorersPage}
-        />
-        <Route path="/leagues/:leagueId/media" component={MediaPage} />
-        <Route path="/leagues/:leagueId/rules" component={RulesPage} />
         <Route component={LandingPage} />
       </Switch>
     </div>

544187c3ad9d0b4f031f94d288a8f759b9965021
Patrik Turi <patrik.turi.0xff@gmail.com>
2020-10-31 21:19:37 +0100

    feat: remove old member site

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,15 +1,13 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { PublicSite } from './public/PublicSite';
-import { MemberSite } from './members/MemberSite';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Dashboard } from './members/Dashboard';
+import { PublicSite } from './public/PublicSite';
 
 export function Site() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/members" component={MemberSite} />
           <Route path="/dashboard" component={Dashboard} />
           <Route component={PublicSite} />
         </Switch>
